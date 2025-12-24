35e649399128e798f93a8b5ace2f95f547d9069c
Maltina Basse <maltinabasse@gmail.com>
2025-12-24 23:07:51 +0100

    feat: system packages (#4)
    * feat: add signal-desktop
    
    * feat: add fluffychat

--- flake.nix
+++ flake.nix
@@ -145,6 +145,8 @@
         environment.systemPackages = with pkgs; [
         #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
         #  wget
+          signal-desktop
+          fluffychat
         ];
 
         # Some programs need SUID wrappers, can be configured further or are
