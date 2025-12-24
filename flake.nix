ad82cc264aa338007c7ce342b6281abec790de7d
Maltina Basse <maltinabasse@gmail.com>
2025-12-24 21:40:41 +0100

    fix: warnings (#3)

--- flake.nix
+++ flake.nix
@@ -86,12 +86,9 @@
           LC_TIME = "de_DE.UTF-8";
         };
 
-        # Enable the X11 windowing system.
-        services.xserver.enable = true;
-
         # Enable the GNOME Desktop Environment.
-        services.xserver.displayManager.gdm.enable = true;
-        services.xserver.desktopManager.gnome.enable = true;
+        services.displayManager.gdm.enable = true;
+        services.desktopManager.gnome.enable = true;
 
         # Configure keymap in X11
         services.xserver.xkb = {
