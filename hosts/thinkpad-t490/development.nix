d5fc4710d32276bee0e1ef2048b1b7bc38640e78
Maltina Basse <maltinabasse@gmail.com>
2026-06-11 19:58:33 +0200

    revert: remove system-wide env vars, use project-level shell.nix instead

--- hosts/thinkpad-t490/development.nix
+++ hosts/thinkpad-t490/development.nix
@@ -20,16 +20,7 @@
     rustlings
     rust-analyzer
     gcc
-    pkg-config
-    openssl
   ];
-
-  # Set up environment variables for OpenSSL to work with Rust builds
-  environment.variables = {
-    PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
-    OPENSSL_DIR = "${pkgs.openssl.dev}";
-    LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath [ pkgs.openssl ]}";
-  };
   
   # Docker and Virtualization
   virtualisation.docker.enable = true;
